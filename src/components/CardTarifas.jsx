d611b43a08b7b207832df414c0bc600768a7ac10
Ezequiel <ezegonza724@gmail.com>
2025-04-07 17:27:35 -0300

    CambiosTarifas

--- src/components/CardTarifas.jsx
+++ src/components/CardTarifas.jsx
@@ -17,7 +17,7 @@ export const CardTarifas = ({item}) => {
         <h4 className="text-start text-break fs-4 text-black">{item.nombre}</h4>
         <span className="border-top border-1 border-secondary w-100 mb-3"></span>
         <span className="text-black">Auto: {item.auto && item.auto.includes("consultar") ? item.auto : <b className="text-success alter-font">${item.auto}</b>}</span>
-        <span className="text-black">Fiat Dobló/Kangoo: {item.fiat_doblo_kangoo && item.fiat_doblo_kangoo.includes("consultar") ? item.fiat_doblo_kangoo : <b className="text-success alter-font">${item.fiat_doblo_kangoo}</b>}</span>
+        <span className="text-black">Fiat Doblo: {item.fiat_doblo_kangoo && item.fiat_doblo_kangoo.includes("consultar") ? item.fiat_doblo_kangoo : <b className="text-success alter-font">${item.fiat_doblo_kangoo}</b>}</span>
         <span className="text-black">Nueva Kangoo: {item.nueva_kangoo && item.nueva_kangoo.includes("consultar") ? item.nueva_kangoo : <b className="text-success alter-font">${item.nueva_kangoo}</b>}</span>
         <span className="text-black">Mercedes Benz Vito: {item.mercedes_benz_vito && item.mercedes_benz_vito.includes("consultar") ? <b className="text-secondary fw-light">{item.mercedes_benz_vito}</b> : <b className="text-success alter-font">${item.mercedes_benz_vito}</b>}</span>
         <span className="text-black">Hiace/Sprinter: {item.hiace_sprinter && item.hiace_sprinter.includes("consultar") ? <b className="text-secondary fw-light">{item.hiace_sprinter}</b> : <b className="text-success alter-font">${item.hiace_sprinter}</b>}</span>
